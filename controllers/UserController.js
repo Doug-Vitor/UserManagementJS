87214b4313b894405ab7cd8ea80d85c8188dc032
Douglas <douglasvitor.dev@gmail.com>
2022-06-22 08:12:08 -0300

    Section 3.: Error fix and ternary operator in controller

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -31,7 +31,6 @@ class UserController {
                 return (item.name === 'photo') ? item : false;
             })
 
-            fileReader.readAsDataURL(photo[0].files[0]);
             fileReader.onload = () => {
                 resolve(fileReader.result)
             };
@@ -39,6 +38,9 @@ class UserController {
             fileReader.onerror = (error) => {
                 reject(error);
             }
+
+            let file = photo[0].files[0];
+            file ? fileReader.readAsDataURL(file) : resolve('dist/img/boxed-bg.jpg');
         });
     }
 
@@ -47,24 +49,27 @@ class UserController {
 
         [...this._formEl.elements].forEach((field) => {
             user[field.name] = field.value;
+
+            if (field.name === 'admin') user[field.name] = field.checked;
         });
         
         return new User(user.name, user.gender, user.birth, user.country, user.email, user.password, user.photo, user.admin);
     }
 
     insertToTable(userData) {
-        this._tableEl.innerHTML += `
-            <tr>
-                <td><img src="${userData.photo}" alt="User Image" class="img-circle img-sm"></td>
+        let tr = document.createElement('tr');
+        tr.innerHTML = `
+            <td><img src="${userData.photo}" alt="User Image" class="img-circle img-sm"></td>
                 <td>${userData.name}</td>
                 <td>${userData.email}</td>
-                <td>${userData.admin}</td>
+                <td>${userData.admin ? 'Sim' : 'Não'}</td>
                 <td>${userData.birth}</td>
             <td>
                 <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
                 <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
             </td>
-        </tr>
         `
+
+        this._tableEl.appendChild(tr);
     }
 }
